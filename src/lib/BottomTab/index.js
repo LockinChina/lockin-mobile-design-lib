6204bac39f886c4b75c460a009e224f2eccb3df9
tanhancheng <hancheng.tan@lockinchina.com>
2019-11-21 11:17:43 +0800

    commit index

--- src/lib/BottomTab/index.js
+++ src/lib/BottomTab/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import PropTypes from 'prop-types';
 
 const Tab = styled.div`
@@ -13,23 +13,53 @@ const Tab = styled.div`
   position: absolute;
   bottom: 0;
   left: 0;
-  .tab-item {
-    height: 50px;
-    width: 25%;
-    display: flex;
-    flex-direction: column;
-    height: 50px;
-    align-items: center;
-    justify-items: center;
-    > div:first-child:before {
-      font-size: 20px;
-    }
-    > div:last-child {
-      width: 100%;
-      text-align: center;
-      font-size: 14px;
-      margin-top: 5px;
-    }
+`;
+
+const TabItem = styled.div`
+  height: 50px;
+  width: 25%;
+  display: flex;
+  flex-direction: column;
+  height: 50px;
+  align-items: center;
+  justify-items: center;
+  position: relative;
+  > div:first-child:before {
+    font-size: 20px;
+  }
+  > div:last-child {
+    width: 100%;
+    text-align: center;
+    font-size: 14px;
+    margin-top: 5px;
+  }
+  ${props =>
+    props.isTip &&
+    css`
+      &:after {
+        position: absolute;
+        content: '';
+        top: 5px;
+        right: 15px;
+        width: 6px;
+        height: 6px;
+        border-radius: 50%;
+        background-color: #fb3d3d;
+      }
+    `}
+  .num-tip {
+    position: absolute;
+    top: 0;
+    right: 15px;
+    text-align: center;
+    width: 16px;
+    height: 16px;
+    line-height: 16px;
+    font-size: 8px;
+    color: #fff;
+    background-color: #fb3d3d;
+    display: inline-block;
+    border-radius: 50%;
   }
 `;
 
@@ -38,7 +68,7 @@ function Index(props) {
   return (
     <Tab>
       {listData.map((item, index) => (
-        <div key={`listData${index + 1}`} className="tab-item">
+        <TabItem key={`listData${index + 1}`} isTip={item.isTip}>
           <div
             className={`lockinu ${
               index === selectIndex ? item.selectedIconName : item.iconName
@@ -48,7 +78,8 @@ function Index(props) {
           <div style={{ color: `${index === selectIndex ? 'blue' : '#ccc'}` }}>
             {item.name}
           </div>
-        </div>
+          {item.unReadNum && <span className="num-tip">{item.unReadNum}</span>}
+        </TabItem>
       ))}
     </Tab>
   );
@@ -58,10 +89,20 @@ export default Index;
 
 Index.defaultProps = {
   listData: [
-    { name: '雇主邀约', iconName: 'iconshipin', selectedIconName: 'iconxinxi' },
+    {
+      name: '雇主邀约',
+      iconName: 'iconshipin',
+      selectedIconName: 'iconxinxi',
+      unReadNum: 10,
+    },
     { name: '找工作', iconName: 'iconshipin', selectedIconName: 'iconxinxi' },
     { name: '活动', iconName: 'iconshipin', selectedIconName: 'iconxinxi' },
-    { name: '我的', iconName: 'iconshipin', selectedIconName: 'iconxinxi' },
+    {
+      name: '我的',
+      iconName: 'iconshipin',
+      selectedIconName: 'iconxinxi',
+      isTip: true,
+    },
   ],
 };
 
